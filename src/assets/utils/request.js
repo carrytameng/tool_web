f2d95a9fd9516c2d5d8cc9f4f183ca73797a58bb
guhongxin <1204738681@qq.com>
2019-09-05 22:39:43 +0800

    新增requset

---
+++ src/assets/utils/request.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import {
+  Message
+} from 'element-ui'
+const server = axios.create({
+  baseURL: process.env.baseUrl
+  // timeout: 30000
+})
+
+// 添加请求拦截器
+server.interceptors.request.use(function (config) {
+  console.log('在发送请求之前', config)
+  // 在发送请求之前做些什么
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+});
+
+// 添加响应拦截器
+server.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  console.log('对响应数据', response)
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  if (/timeout/.test(error.message)) {
+    message('请求超时, 请重新连接')
+  }
+  return Promise.reject(error)
+})
+let message = function(info) {
+  Message({
+    message: info,
+    type: 'error',
+    duration: 3.5 * 1000
+  })
+}
+export default server
